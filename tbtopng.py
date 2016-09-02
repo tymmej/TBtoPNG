30e5382137691cd409dfdb668ada61ce521dd70b
Jakub Tymejczyk <jakub@tymejczyk.pl>
2016-09-02 20:52:16 +0200

    Some old change

--- tbtopng.py
+++ tbtopng.py
@@ -1,15 +1,24 @@
+#!/usr/bin/env python
 from glob import glob as ls
 from os.path import join
 from re import match
+from re import escape
 from os import rename
 from os import system
+#from numpy import unique
+import numpy
+import sys
 
+Dir = "/Users/tymmej/Downloads/"
+mapName = sys.argv[1]
+baseDir = join(Dir, mapName)
+setDir = join(baseDir, "set")
 
-baseDir = '/tmp/trekbuddy/01-SubMap/'
-setDir = '/tmp/trekbuddy/01-SubMap/set/'
-
-listOfFiles = ls(join(setDir, '*.png'))
-renamePattern = join(setDir, r'SubMap01_(\d+)_(\d+).png')
+print(setDir)
+listOfFiles = ls(join(setDir, "*.png"))
+#print(listOfFiles)
+regex = r'' + escape(mapName) + r"_(\d+)_(\d+).png"
+renamePattern = join(setDir, regex)
 
 firstNumList = []
 for item in listOfFiles:
@@ -22,20 +31,21 @@ for item in listOfFiles:
 
         firstNumList.append(firstNum)
 
-        newFileName = 'SubMap01_' + firstNum + '_' + secondNum + '.png'
+        newFileName = mapName + "_" + firstNum + "_" + secondNum + ".png"
         newFilePath = join(setDir, newFileName)
-        print newFileName
+        #print(newFileName)
         rename(filePath, newFilePath)
 
-firstNumList = unique(firstNumList)
+firstNumList = numpy.unique(firstNumList)
+
 for item in firstNumList:
-    listOfFiles = ls(join(baseDir, 'SubMap01_' + item + '*'))
+    listOfFiles = ls(join(baseDir, mapName + "_" + item + "*"))
     numOfFiles = len(listOfFiles)
-    cmd = "montage " + join(setDir,"SubMap01_" + item + "_*.png") + " -mode Concatenate -tile 1x " + join(baseDir, "SubMap01_"+item+".png")
-    print cmd
+    cmd = "montage \"" + join(setDir, mapName + "_" + item + "_*.png") + "\" -mode Concatenate -tile 1x \"" + join(baseDir, mapName + "_" + item + ".png") + "\""
+    print(cmd)
     system(cmd)
 
 
-cmd = 'montage ' + join(baseDir, "SubMap01_*.png") + " -mode Concatenate -tile x1 " + join(baseDir, 'SubMap.png')
-print cmd
+cmd = "montage \"" + join(baseDir, mapName + "_*.png") + "\" -mode Concatenate -tile x1 \"" + join(baseDir, mapName + '.png') + "\""
+print(cmd)
 system(cmd)
